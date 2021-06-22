2a58cb71f06891c21a65917f0e0757e7eb103a38
sparshsharma2510 <sparsh.19573@sscbs.du.ac.in>
2021-06-22 15:09:07 +0530

    First Commit

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -43,7 +43,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <div>
+    <React.Fragment>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
@@ -66,7 +66,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </React.Fragment>
   );
 };
 
